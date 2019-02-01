2f70bd5777c37b3bcb7425a7e4bceb0a509802a7
Sakakura <njb-C18256@sist.ac.jp>
2019-02-01 09:35:36 +0900

    First Commit

---
+++ フォームデザイン1/フォームデザイン1/DataBase.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace フォームデザイン1
+{
+    [Serializable]
+    class DataBase
+    {
+        private readonly List<Book> _books = new List<Book>();
+        private readonly List<Member> _members = new List<Member>();
+        DataBase()
+        {
+        }
+
+        public bool AddBook(Book book)
+        {
+            if (GetBookByIsbn(book.Isbn) != null) return false;
+            _books.Add(book);
+            return true;
+        }
+
+        public bool AddMember(Member member)
+        {
+            if (GetMemberByNumber(member.Number) == null)
+            {
+                _members.Add(member);
+                return true;
+            }
+            return false;
+        }
+
+        public Member GetMemberByNumber(int number)
+        {
+            foreach (var member in _members)
+            {
+                if (member.Number == number)
+                    return member;
+            }
+            return null;
+        }
+
+        public List<Book> GetBooksByTitle(string title)
+        {
+            var result = new List<Book>();
+            foreach (var book in _books)
+            {
+                if(book.Title.IndexOf(title, StringComparison.Ordinal)!=-1)
+                    result.Add(book);
+            }
+            return result;
+        }
+
+        public Book GetBookByIsbn(int isbn)
+        {
+            foreach (var book in _books)
+            {
+                if (book.Isbn == isbn)
+                    return book;
+            }
+            return null;
+        }
+    }
+
+    class Book
+    {
+        Book()
+        {
+
+        }
+        public int Isbn { get; set; }
+        public string Title { get; set; }
+        public string Actor { get; set; }
+        public string Company { get; set; }
+        public DateTime IssueDate { get; set; }
+    }
+
+    class Member
+    {
+        Member()
+        {
+
+        }
+        public int Number { get; set; }
+        public string Name { get; set; }
+        public string Tel { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public DateTime BarthDay { get; set; }
+    }
+}
---
+++ フォームデザイン1/フォームデザイン1/bookRegist.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace フォームデザイン1
+{
+    partial class bookRegist
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtIsbn = new System.Windows.Forms.TextBox();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.txtActor = new System.Windows.Forms.TextBox();
+            this.txtCom = new System.Windows.Forms.TextBox();
+            this.dateIssue = new System.Windows.Forms.DateTimePicker();
+            this.btnRegist = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ISBNコード";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "タイトル";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 20);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "著者名";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 110);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 20);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "発行年月日";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 140);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 20);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "発行会社";
+            // 
+            // txtIsbn
+            // 
+            this.txtIsbn.Location = new System.Drawing.Point(155, 9);
+            this.txtIsbn.Name = "txtIsbn";
+            this.txtIsbn.Size = new System.Drawing.Size(200, 26);
+            this.txtIsbn.TabIndex = 1;
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(155, 41);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(200, 26);
+            this.txtTitle.TabIndex = 1;
+            // 
+            // txtActor
+            // 
+            this.txtActor.Location = new System.Drawing.Point(155, 73);
+            this.txtActor.Name = "txtActor";
+            this.txtActor.Size = new System.Drawing.Size(200, 26);
+            this.txtActor.TabIndex = 1;
+            // 
+            // txtCom
+            // 
+            this.txtCom.Location = new System.Drawing.Point(155, 137);
+            this.txtCom.Name = "txtCom";
+            this.txtCom.Size = new System.Drawing.Size(200, 26);
+            this.txtCom.TabIndex = 1;
+            // 
+            // dateIssue
+            // 
+            this.dateIssue.Location = new System.Drawing.Point(155, 105);
+            this.dateIssue.Name = "dateIssue";
+            this.dateIssue.Size = new System.Drawing.Size(200, 26);
+            this.dateIssue.TabIndex = 2;
+            // 
+            // btnRegist
+            // 
+            this.btnRegist.Location = new System.Drawing.Point(16, 169);
+            this.btnRegist.Name = "btnRegist";
+            this.btnRegist.Size = new System.Drawing.Size(339, 33);
+            this.btnRegist.TabIndex = 3;
+            this.btnRegist.Text = "登録";
+            this.btnRegist.UseVisualStyleBackColor = true;
+            // 
+            // bookRegist
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(369, 219);
+            this.Controls.Add(this.btnRegist);
+            this.Controls.Add(this.dateIssue);
+            this.Controls.Add(this.txtCom);
+            this.Controls.Add(this.txtActor);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.txtIsbn);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "bookRegist";
+            this.Text = "bookRegist";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtIsbn;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.TextBox txtActor;
+        private System.Windows.Forms.TextBox txtCom;
+        private System.Windows.Forms.DateTimePicker dateIssue;
+        private System.Windows.Forms.Button btnRegist;
+    }
+}
