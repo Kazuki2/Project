0f7c01c8789528683f0baf3699bf3a9314dbee42
C18107 sato <C18107 sato@LAPTOP-CEP3C42J>
2019-02-05 09:27:43 +0900

    マージン前コミット

--- フォームデザイン1/フォームデザイン1/bookRegist.Designer.cs
+++ フォームデザイン1/フォームデザイン1/bookRegist.Designer.cs
@@ -43,97 +43,100 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Location = new System.Drawing.Point(13, 11);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(83, 20);
+            this.label1.Size = new System.Drawing.Size(86, 18);
             this.label1.TabIndex = 0;
             this.label1.Text = "ISBNコード";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(13, 44);
+            this.label2.Location = new System.Drawing.Point(14, 40);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(56, 20);
+            this.label2.Size = new System.Drawing.Size(61, 18);
             this.label2.TabIndex = 0;
             this.label2.Text = "タイトル";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(13, 76);
+            this.label3.Location = new System.Drawing.Point(14, 68);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(57, 20);
+            this.label3.Size = new System.Drawing.Size(62, 18);
             this.label3.TabIndex = 0;
             this.label3.Text = "著者名";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(13, 110);
+            this.label4.Location = new System.Drawing.Point(14, 99);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(89, 20);
+            this.label4.Size = new System.Drawing.Size(98, 18);
             this.label4.TabIndex = 0;
             this.label4.Text = "発行年月日";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(13, 140);
+            this.label5.Location = new System.Drawing.Point(14, 126);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(73, 20);
+            this.label5.Size = new System.Drawing.Size(80, 18);
             this.label5.TabIndex = 0;
             this.label5.Text = "発行会社";
             // 
             // txtIsbn
             // 
-            this.txtIsbn.Location = new System.Drawing.Point(155, 9);
+            this.txtIsbn.Location = new System.Drawing.Point(172, 8);
             this.txtIsbn.Name = "txtIsbn";
-            this.txtIsbn.Size = new System.Drawing.Size(200, 26);
+            this.txtIsbn.Size = new System.Drawing.Size(222, 25);
             this.txtIsbn.TabIndex = 1;
+            this.txtIsbn.TextChanged += new System.EventHandler(this.txtIsbn_TextChanged);
             // 
             // txtTitle
             // 
-            this.txtTitle.Location = new System.Drawing.Point(155, 41);
+            this.txtTitle.Location = new System.Drawing.Point(172, 37);
             this.txtTitle.Name = "txtTitle";
-            this.txtTitle.Size = new System.Drawing.Size(200, 26);
+            this.txtTitle.Size = new System.Drawing.Size(222, 25);
             this.txtTitle.TabIndex = 1;
+            this.txtTitle.TextChanged += new System.EventHandler(this.txtTitle_TextChanged);
             // 
             // txtActor
             // 
-            this.txtActor.Location = new System.Drawing.Point(155, 73);
+            this.txtActor.Location = new System.Drawing.Point(172, 66);
             this.txtActor.Name = "txtActor";
-            this.txtActor.Size = new System.Drawing.Size(200, 26);
+            this.txtActor.Size = new System.Drawing.Size(222, 25);
             this.txtActor.TabIndex = 1;
             // 
             // txtCom
             // 
-            this.txtCom.Location = new System.Drawing.Point(155, 137);
+            this.txtCom.Location = new System.Drawing.Point(172, 123);
             this.txtCom.Name = "txtCom";
-            this.txtCom.Size = new System.Drawing.Size(200, 26);
+            this.txtCom.Size = new System.Drawing.Size(222, 25);
             this.txtCom.TabIndex = 1;
             // 
             // dateIssue
             // 
-            this.dateIssue.Location = new System.Drawing.Point(155, 105);
+            this.dateIssue.Location = new System.Drawing.Point(172, 94);
             this.dateIssue.Name = "dateIssue";
-            this.dateIssue.Size = new System.Drawing.Size(200, 26);
+            this.dateIssue.Size = new System.Drawing.Size(222, 25);
             this.dateIssue.TabIndex = 2;
             // 
             // btnRegist
             // 
-            this.btnRegist.Location = new System.Drawing.Point(16, 169);
+            this.btnRegist.Location = new System.Drawing.Point(18, 152);
             this.btnRegist.Name = "btnRegist";
-            this.btnRegist.Size = new System.Drawing.Size(339, 33);
+            this.btnRegist.Size = new System.Drawing.Size(377, 30);
             this.btnRegist.TabIndex = 3;
             this.btnRegist.Text = "登録";
             this.btnRegist.UseVisualStyleBackColor = true;
+            this.btnRegist.Click += new System.EventHandler(this.btnRegist_Click);
             // 
             // bookRegist
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(369, 219);
+            this.ClientSize = new System.Drawing.Size(410, 197);
             this.Controls.Add(this.btnRegist);
             this.Controls.Add(this.dateIssue);
             this.Controls.Add(this.txtCom);
