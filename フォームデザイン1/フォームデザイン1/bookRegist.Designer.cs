67c6d8119a097f5a22410d1902119269d1a7f95a
C18107 sato <C18107 sato@LAPTOP-CEP3C42J>
2019-02-05 09:42:19 +0900

    マージ後コミット

--- フォームデザイン1/フォームデザイン1/bookRegist.Designer.cs
+++ フォームデザイン1/フォームデザイン1/bookRegist.Designer.cs
@@ -38,105 +38,137 @@
             this.txtCom = new System.Windows.Forms.TextBox();
             this.dateIssue = new System.Windows.Forms.DateTimePicker();
             this.btnRegist = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 11);
+            this.label1.Font = new System.Drawing.Font("ＭＳ ゴシック", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(249, 414);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(86, 18);
+            this.label1.Size = new System.Drawing.Size(195, 36);
             this.label1.TabIndex = 0;
             this.label1.Text = "ISBNコード";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(14, 40);
+            this.label2.Font = new System.Drawing.Font("ＭＳ ゴシック", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(28, 142);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(61, 18);
+            this.label2.Size = new System.Drawing.Size(159, 36);
             this.label2.TabIndex = 0;
             this.label2.Text = "タイトル";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(14, 68);
+            this.label3.Font = new System.Drawing.Font("ＭＳ ゴシック", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.Location = new System.Drawing.Point(28, 250);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(62, 18);
+            this.label3.Size = new System.Drawing.Size(123, 36);
             this.label3.TabIndex = 0;
             this.label3.Text = "著者名";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(14, 99);
+            this.label4.Font = new System.Drawing.Font("ＭＳ ゴシック", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label4.Location = new System.Drawing.Point(477, 250);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(98, 18);
+            this.label4.Size = new System.Drawing.Size(195, 36);
             this.label4.TabIndex = 0;
             this.label4.Text = "発行年月日";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(14, 126);
+            this.label5.Font = new System.Drawing.Font("ＭＳ ゴシック", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label5.Location = new System.Drawing.Point(477, 142);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(80, 18);
+            this.label5.Size = new System.Drawing.Size(159, 36);
             this.label5.TabIndex = 0;
             this.label5.Text = "発行会社";
             // 
             // txtIsbn
             // 
-            this.txtIsbn.Location = new System.Drawing.Point(172, 8);
+            this.txtIsbn.Font = new System.Drawing.Font("ＭＳ 明朝", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txtIsbn.Location = new System.Drawing.Point(450, 408);
             this.txtIsbn.Name = "txtIsbn";
-            this.txtIsbn.Size = new System.Drawing.Size(222, 25);
+            this.txtIsbn.Size = new System.Drawing.Size(222, 47);
             this.txtIsbn.TabIndex = 1;
-            this.txtIsbn.TextChanged += new System.EventHandler(this.txtIsbn_TextChanged);
             // 
             // txtTitle
             // 
-            this.txtTitle.Location = new System.Drawing.Point(172, 37);
+            this.txtTitle.Font = new System.Drawing.Font("ＭＳ 明朝", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txtTitle.Location = new System.Drawing.Point(193, 139);
             this.txtTitle.Name = "txtTitle";
-            this.txtTitle.Size = new System.Drawing.Size(222, 25);
+            this.txtTitle.Size = new System.Drawing.Size(278, 47);
             this.txtTitle.TabIndex = 1;
-            this.txtTitle.TextChanged += new System.EventHandler(this.txtTitle_TextChanged);
             // 
             // txtActor
             // 
-            this.txtActor.Location = new System.Drawing.Point(172, 66);
+            this.txtActor.Font = new System.Drawing.Font("ＭＳ 明朝", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txtActor.Location = new System.Drawing.Point(193, 244);
             this.txtActor.Name = "txtActor";
-            this.txtActor.Size = new System.Drawing.Size(222, 25);
+            this.txtActor.Size = new System.Drawing.Size(278, 47);
             this.txtActor.TabIndex = 1;
             // 
             // txtCom
             // 
-            this.txtCom.Location = new System.Drawing.Point(172, 123);
+            this.txtCom.Font = new System.Drawing.Font("ＭＳ 明朝", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txtCom.Location = new System.Drawing.Point(684, 139);
             this.txtCom.Name = "txtCom";
-            this.txtCom.Size = new System.Drawing.Size(222, 25);
+            this.txtCom.Size = new System.Drawing.Size(310, 47);
             this.txtCom.TabIndex = 1;
             // 
             // dateIssue
             // 
-            this.dateIssue.Location = new System.Drawing.Point(172, 94);
+            this.dateIssue.Font = new System.Drawing.Font("ＭＳ 明朝", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.dateIssue.Location = new System.Drawing.Point(684, 245);
             this.dateIssue.Name = "dateIssue";
-            this.dateIssue.Size = new System.Drawing.Size(222, 25);
+            this.dateIssue.Size = new System.Drawing.Size(310, 43);
             this.dateIssue.TabIndex = 2;
             // 
             // btnRegist
             // 
-            this.btnRegist.Location = new System.Drawing.Point(18, 152);
+            this.btnRegist.Font = new System.Drawing.Font("HG行書体", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnRegist.Location = new System.Drawing.Point(743, 382);
             this.btnRegist.Name = "btnRegist";
-            this.btnRegist.Size = new System.Drawing.Size(377, 30);
+            this.btnRegist.Size = new System.Drawing.Size(212, 95);
             this.btnRegist.TabIndex = 3;
             this.btnRegist.Text = "登録";
             this.btnRegist.UseVisualStyleBackColor = true;
-            this.btnRegist.Click += new System.EventHandler(this.btnRegist_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("HG行書体", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button1.Location = new System.Drawing.Point(54, 397);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(164, 64);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "戻る";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("HGS教科書体", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label6.Location = new System.Drawing.Point(356, 22);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(248, 56);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "書籍登録";
             // 
             // bookRegist
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(410, 197);
+            this.ClientSize = new System.Drawing.Size(1007, 517);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btnRegist);
             this.Controls.Add(this.dateIssue);
             this.Controls.Add(this.txtCom);
@@ -168,5 +200,7 @@
         private System.Windows.Forms.TextBox txtCom;
         private System.Windows.Forms.DateTimePicker dateIssue;
         private System.Windows.Forms.Button btnRegist;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label6;
     }
 }
